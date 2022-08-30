1c1daa3f81f5390e2b03b25a78f7052691d586b0
alvin <alvin@alvin.com>
2022-08-30 14:13:33 +0800

    test2

--- packages/alvin-form/al-form.vue
+++ packages/alvin-form/al-form.vue
@@ -1,27 +1,28 @@
 <template>
-	<div class="al-form">
-		<slot />
-	</div>
+  <div class="al-form">
+    <slot />
+  </div>
 </template>
 <script>
 export default {
-  name: 'AlForm',
-  props: ['model', 'rules'],
-  provide () {
+  name: "AlForm",
+  props: ["model", "rules"],
+  provide() {
     return {
-      AlFrom: this
-    }
+      AlFrom: this,
+    };
   },
   methods: {
-    async validate (cb) {
+    async validate(cb) {
       // 校验
+      console.log("asdf");
       const request = this.$children
-        .filter(x => x.prop)
-        .map(child => child.validate())
-      const result = await Promise.all(request)
-      const isValidate = result.every(x => !!x)
-      cb(isValidate)
-    }
-  }
-}
+        .filter((x) => x.prop)
+        .map((child) => child.validate());
+      const result = await Promise.all(request);
+      const isValidate = result.every((x) => !!x);
+      cb(isValidate);
+    },
+  },
+};
 </script>
